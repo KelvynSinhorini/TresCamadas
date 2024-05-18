41133af0e95a0c74d483e6838c02362a6cbddea6
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-18 20:41:20 -0300

    feat: add entidade Entity, Fornecedor, Endereco e Produto

---
+++ src/TresCamadas.Business/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace TresCamadas.Business.Models;
+
+public abstract class Entity
+{
+    protected Entity() 
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+}
---
+++ src/TresCamadas.Business/Models/TipoFornecedor.cs
@@ -0,0 +1,7 @@
+﻿namespace TresCamadas.Business.Models;
+
+public enum TipoFornecedor
+{
+    PessoaFisica = 1,
+    PessoaJuridica = 2
+}
