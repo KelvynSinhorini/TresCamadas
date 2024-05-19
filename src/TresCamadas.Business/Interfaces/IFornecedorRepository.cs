53426d514f95edebf31dc0ae3519e3d73fd0a1b3
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-19 12:47:10 -0300

    feat: definir interfaces do padrão repository

---
+++ src/TresCamadas.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,11 @@
+﻿using TresCamadas.Business.Models;
+
+namespace TresCamadas.Business.Interfaces;
+
+public interface IFornecedorRepository : IRepository<Fornecedor>
+{
+    Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+    Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+
+    Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+}
---
+++ src/TresCamadas.Business/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,9 @@
+﻿using TresCamadas.Business.Models;
+
+namespace TresCamadas.Business.Interfaces;
+public interface IProdutoRepository : IRepository<Produto>
+{
+    Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+    Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+    Task<Produto> ObterProdutoFornecedor(Guid id);
+}
