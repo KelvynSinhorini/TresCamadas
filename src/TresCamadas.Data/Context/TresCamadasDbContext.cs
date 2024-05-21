7b05149bf5d984d5720fff235f684d4cb734fc4c
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 13:37:46 -0300

    feat: remover tracking do ef core, e sobreescrever o metodo SaveChangesAsync

--- src/TresCamadas.Data/Context/TresCamadasDbContext.cs
+++ src/TresCamadas.Data/Context/TresCamadasDbContext.cs
@@ -6,6 +6,8 @@ public class TresCamadasDbContext : DbContext
 {
     public TresCamadasDbContext(DbContextOptions<TresCamadasDbContext> options) : base(options)
     {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
     }
 
     public DbSet<Produto> Produtos { get; set; }
@@ -24,5 +26,23 @@ public class TresCamadasDbContext : DbContext
         foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataCadastro").IsModified = false;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
