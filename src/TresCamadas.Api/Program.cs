234dfc93ed392b9c277ae3a638203655fadfe3df
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 15:09:34 -0300

    feat: limpar TresCamadas.Api

--- src/TresCamadas.Api/Program.cs
+++ src/TresCamadas.Api/Program.cs
@@ -3,10 +3,8 @@ using TresCamadas.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +15,6 @@ builder.Services.AddDbContext<TresCamadasDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
