df2ae3444b79be9b5d486dfabb7039aa2f289290
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 14:55:40 -0300

    feat: implementar repositorio por agregação

---
+++ src/TresCamadas.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TresCamadas.Business.Interfaces;
+using TresCamadas.Business.Models;
+using TresCamadas.Data.Context;
+
+namespace TresCamadas.Data.Repository;
+
+public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+{
+    public FornecedorRepository(TresCamadasDbContext context) : base(context) { }
+
+    public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+    {
+        return await Db.Fornecedores.AsNoTracking()
+            .Include(c => c.Endereco)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+    {
+        return await Db.Fornecedores.AsNoTracking()
+            .Include(c => c.Produtos)
+            .Include(c => c.Endereco)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+    {
+        return await Db.Enderecos.AsNoTracking()
+            .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+    }
+
+    public async Task RemoverEnderecoFornecedor(Endereco endereco)
+    {
+        Db.Enderecos.Remove(endereco);
+        await SaveChanges();
+    }
+}
