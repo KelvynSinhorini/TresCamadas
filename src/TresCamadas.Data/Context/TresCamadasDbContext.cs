4ec48789a9ac8d7c973daad9ab7fe9cfdf3bb2cc
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 12:16:59 -0300

    feat: add DbContext

---
+++ src/TresCamadas.Data/Context/TresCamadasDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TresCamadas.Business.Models;
+
+namespace TresCamadas.Data.Context;
+public class TresCamadasDbContext : DbContext
+{
+    public TresCamadasDbContext(DbContextOptions<TresCamadasDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+}
