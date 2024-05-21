4f2413c21aca07448be9166a6604d850920f4762
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 15:50:15 -0300

    feat: Add AutoMapperConfig

---
+++ src/TresCamadas.Api/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TresCamadas.Api.ViewModels;
+using TresCamadas.Business.Models;
+
+namespace TresCamadas.Api.Configurations;
+
+public class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+        CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+        CreateMap<ProdutoViewModel, Produto>();
+
+        CreateMap<Produto, ProdutoViewModel>()
+            .ForMember(dest => dest.NomeFornecedor, opt => opt.MapFrom(src => src.Fornecedor.Nome));
+    }
+}
