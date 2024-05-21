a29ffe856bd14b21a9389ace906089383572a319
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-21 15:52:47 -0300

    feat: configurar o autoMapper na program.cs

--- src/TresCamadas.Api/Program.cs
+++ src/TresCamadas.Api/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<TresCamadasDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
