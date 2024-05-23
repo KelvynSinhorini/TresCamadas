a3d3780230aed64df1056e2ce307e99202df4a76
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-22 23:43:28 -0300

    feat: implementar injeções de dependencia

---
+++ src/TresCamadas.Api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using TresCamadas.Business.Interfaces;
+using TresCamadas.Business.Notificacoes;
+using TresCamadas.Business.Services;
+using TresCamadas.Data.Context;
+using TresCamadas.Data.Repository;
+
+namespace TresCamadas.Api.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+    {
+        // Data
+        services.AddScoped<TresCamadasDbContext>();
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+
+        // Business
+        services.AddScoped<IFornecedorService, FornecedorService>();
+        services.AddScoped<IProdutoService, ProdutoService>();
+        services.AddScoped<INotificador, Notificador>();
+
+        return services;
+    }
+}
--- src/TresCamadas.Api/Program.cs
+++ src/TresCamadas.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TresCamadas.Api.Configurations;
 using TresCamadas.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,8 @@ builder.Services.AddDbContext<TresCamadasDbContext>(options =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
