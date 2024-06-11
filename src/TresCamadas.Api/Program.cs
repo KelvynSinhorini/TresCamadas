cfd9f6ad7e85e9ee8babd57bf2faddc1262f660c
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-06-10 23:17:56 -0300

    feat: adicionar option SuppressModelStateInvalidFilter

--- src/TresCamadas.Api/Program.cs
+++ src/TresCamadas.Api/Program.cs
@@ -4,7 +4,12 @@ using TresCamadas.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
